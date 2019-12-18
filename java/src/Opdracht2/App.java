023615badd2d02804f6a65600108ae383e332360
sThommB <>
2019-12-18 15:02:30 +0100

    Added default values && start stop function

--- java/src/Opdracht2/App.java
+++ java/src/Opdracht2/App.java
@@ -2,6 +2,10 @@ package Opdracht2;
 
 public class App {
     public static void main(String[] args) {
-
+        Motor motor = new Motor();
+        motor.start();
+        System.out.println(motor.isRunning());
+        motor.stop();
+        System.out.println(motor.isRunning());
     }
 }
--- java/src/Opdracht2/Motor.java
+++ java/src/Opdracht2/Motor.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Motor {
 
-    private boolean isRunning;
-    private int roundsPerMinute;
+    private boolean isRunning = false;
+    private int roundsPerMinute = 0;
     private float fuel;
 
     public Motor() {
@@ -13,11 +13,23 @@ public class Motor {
     }
 
     public void start() {
-
+        isRunning = true;
+        roundsPerMinute = 800;
     }
 
     public void stop() {
-
+        isRunning = false;
     }
 
+    public void pushGasPedal() {
+        // roundsPerMinute ++
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public int getRoundsPerMinute() {
+        return roundsPerMinute;
+    }
 }
