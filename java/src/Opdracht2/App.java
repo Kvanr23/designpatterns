cc7e5e0af0bf278eb8d9ff3943e49e13f77fc944
sThommB <>
2019-12-18 14:50:40 +0100

    Start opdracht 2

---
+++ java/src/Opdracht2/App.java
@@ -0,0 +1,7 @@
+package Opdracht2;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java/src/Opdracht2/Motor.java
@@ -0,0 +1,23 @@
+package Opdracht2;
+import java.util.Random;
+
+
+public class Motor {
+
+    private boolean isRunning;
+    private int roundsPerMinute;
+    private float fuel;
+
+    public Motor() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+
+}
